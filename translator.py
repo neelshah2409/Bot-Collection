3866909ee50de4eec70044777369dbb150ad01f1
AKSHAY MISHRA <72158107+akshaymishra7@users.noreply.github.com>
2022-01-03 20:35:01 +0530

    translator in python
    A python used to convert text to desired language.

---
+++ translator.py
@@ -0,0 +1,9 @@
+from googletrans import Translator
+
+sentence=str(input("say......"))
+
+translator=Translator()
+
+translated_sentence=translator.translate(sentence,src='en,dest='ca)
+
+print(translated_sentence.text)
