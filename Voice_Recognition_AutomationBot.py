19ac1161af16ab6fb41fdbe16c9d469b67d8bbe5
shubx08 <shubham.patel.btech2020@sitpune.edu.in>
2022-01-19 09:27:41 +0530

    login with your email

--- Voice_Recognition_AutomationBot.py
+++ Voice_Recognition_AutomationBot.py
@@ -2,4 +2,8 @@ import smtplib
 from email.message import EmailMessage
 
 server = smtplib.SMTP("smtp.gmail.com",587)
+server.starttls()
 
+server_login_mail = "youremail@gmail.com"#enter your own email here,if it raises SMTPAuthenticationError(code, resp) then allow less secure apps in your email security settings to execute this script
+server_login_password = "yourpassword13@$"#enter your own email password here
+server.login(server_login_mail, server_login_password)
