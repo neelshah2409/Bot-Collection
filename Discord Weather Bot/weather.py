3eb82e2cf4b076790369d17da9b7f1496aa1dd98
Wasim Reja <iwasimreja@gmail.com>
2022-02-22 01:15:49 +0530

    Made a discord weather bot

---
+++ Discord Weather Bot/weather.py
@@ -0,0 +1,36 @@
+import discord
+
+color = 0x0099ff
+key_features = {
+    'temp' : 'Temperature',
+    'feels_like' : 'Feels Like',
+    'temp_min' : 'Minimum Temperature',
+    'temp_max' : 'Maximum Temperature'
+}
+
+def parse_data(data):
+    del data['humidity']
+    del data['pressure']
+    return data
+
+def weather_message(data, location):
+    location = location.title()
+    message = discord.Embed(
+        title=f'Weather in {location}',
+        color=color
+    )
+    for key in data:
+        message.add_field(
+            name=key_features[key],
+            value=str(data[key]),
+            inline=False
+        )
+    return message
+
+def error_message(location):
+    location = location.title()
+    return discord.Embed(
+        title='Error',
+        description=f'There was an error retrieving weather data for {location}',
+        color=color
+    )
