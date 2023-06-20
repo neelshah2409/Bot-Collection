e0e56bcb24263d7b908c66a0ab48222c532599c7
krishvsoni <sonikrish994@gmail.com>
2023-06-20 13:16:05 +0530

    meme bot modified

--- meme_bot/bot.py
+++ meme_bot/bot.py
@@ -1,41 +1,33 @@
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram import Update, ParseMode
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 import requests
 import os
-
-# load token in env
 from dotenv import load_dotenv
 
 load_dotenv()
 
-# fetch token from env
+# Fetch token from environment variable
 token_key = os.environ["BOT_TOKEN"]
 
 
-def start(update, context):
-    update.message.reply_text("Hey Yo!!! Imma gib u some memez!")
+def start(update: Update, context: CallbackContext):
+    update.message.reply_text("Hey Yo!!! Imma give you some memez!")
 
 
-def get_help(update, context):
-    update.message.reply_text(
-        """
-
+def get_help(update: Update, context: CallbackContext):
+    help_message = """
     /start -> Welcome message
-    /help -> This message; shows avialable messages
-    /meme -> throws some random meme
-
+    /help -> Show available commands
+    /meme -> Get a random meme
     """
-    )
+    update.message.reply_text(help_message)
 
 
-def handle_message(update, context):
-    update.message.reply_text(
-        """
-        Sorry, I did not understand that command.
-        Type \" /help \" to see all possible commands"""
-    )
+def handle_message(update: Update, context: CallbackContext):
+    update.message.reply_text("Sorry, I did not understand that command. Type \"/help\" to see all possible commands.")
 
 
-def get_meme(update, context):
+def get_meme(update: Update, context: CallbackContext):
     API_ENDPOINT = "https://meme-api.herokuapp.com/gimme"
 
     response = requests.get(url=API_ENDPOINT)
@@ -45,22 +37,42 @@ def get_meme(update, context):
         meme_title = json_data["title"]
         meme_url = json_data["url"]
 
-        context.bot.send_photo(chat_id=update.effective_chat.id, photo=meme_url)
-        update.message.reply_text("Title : " + meme_title)
+        context.bot.send_photo(chat_id=update.effective_chat.id, photo=meme_url, caption=f"Title: {meme_title}")
     else:
-        update.message.reply_text("Error, something went wrong.")
+        update.message.reply_text("Error: Something went wrong.")
 
 
-if __name__ == "__main__":
-
+def main():
     updater = Updater(token=token_key, use_context=True)
     dispatcher = updater.dispatcher
 
-    # Command Lists
+    # Command Handlers
     dispatcher.add_handler(CommandHandler("start", start))
     dispatcher.add_handler(CommandHandler("help", get_help))
     dispatcher.add_handler(CommandHandler("meme", get_meme))
-    dispatcher.add_handler(MessageHandler(Filters.text, handle_message))
+    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, handle_message))
 
     updater.start_polling()
     updater.idle()
+
+
+if __name__ == "__main__":
+    main()
+    '''
+Changes made in the code:
+
+Added type hints to function arguments for better code readability.
+Imported the necessary classes (Update, ParseMode) from the telegram module.
+Utilized the CallbackContext in the function signatures to adhere to the latest python-telegram-bot library conventions.
+Modified the help message formatting for better readability.
+Modified the error message to be more descriptive.
+Created a main function to start the bot.
+Improved the MessageHandler filter to exclude commands from being handled as regular text.
+Remember to replace "BOT_TOKEN" with your actual bot token before running the code.
+'''
+
+
+
+
+
+Regenerate response
