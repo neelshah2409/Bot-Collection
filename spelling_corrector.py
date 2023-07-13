bcf5ecc6fe9d2256def4260a67d9489ece3ebd23
9492277468+-Aa <idiotcommerce@gmail.com>
2023-07-13 09:27:57 +0530

    in commections you can add data in to file if you want

--- spelling_corrector.py
+++ spelling_corrector.py
@@ -1,6 +1,15 @@
 from textblob import TextBlob
 
 file1 = open("1.txt", "w+")
+
+
+# Write some text to the file
+#file1.write("original text")
+
+# Reset the file pointer to the beginning of the file
+#file1.seek(0)
+
+
 a = file1.read()
 print("original text : " + str(a))
 b = TextBlob(a)
